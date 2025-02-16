d8254aac1f3c5c4e8d4b8433c99ea1fdbb4391a5
Fernando Mauro <fermaurolf@gmail.com>
2025-02-16 14:01:32 -0600

    I don know!

--- app/Http/Controllers/v1/Migrations/MigrationController.php
+++ app/Http/Controllers/v1/Migrations/MigrationController.php
@@ -8,39 +8,53 @@ use App\Models\Group;
 use App\Models\Level;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class MigrationController extends Controller
 {
     public function runSemesterMigration()
     {
-        $userId = Auth::id();
-        $campusId = Admin::where('user_id', $userId)->first()->campus_id;
+        DB::beginTransaction();
+        try{
 
-        $groups = Group::where('campus_id', $campusId)->get();
-        $levels = Level::where('campus_id', $campusId)->get();
-        
-        $keys = array(
-            "Primero" => "Segundo",
-            "Segundo" => "Tercero",
-            "Tercero" => "Cuarto",
-            "Cuarto" => "Quinto",
-            "Quinto" => "Sexto"
-        );
-
-        foreach($groups as $group) {
-            $currentLevel = $levels->where('id', $group->level_id)->first();
-            $nextLevel = $levels->where('name', $keys[$currentLevel->name])->first();
-
-            if($nextLevel) {
-                $groups->name = $group->name += 100;
-                $group->level_id = $nextLevel->id;
-                $group->save();
+            $userId = Auth::id();
+            $campusId = Admin::where('user_id', $userId)->first()->campus_id;
+    
+            $groups = Group::where('campus_id', $campusId)->get();
+            $levels = Level::where('campus_id', $campusId)->get();
+            
+            $keys = array(
+                "Primero" => "Segundo",
+                "Segundo" => "Tercero",
+                "Tercero" => "Cuarto",
+                "Cuarto" => "Quinto",
+                "Quinto" => "Sexto",
+                "Sexto" => "Egresado"
+            );
+    
+            foreach($groups as $group) {
+                $currentLevel = $levels->where('id', $group->level_id)->first();
+                if($currentLevel->name == "Sexto"){
+                    Log::info('El grupo ' . $group->name . ' no se puede mover de nivel');
+                    continue;
+                }
+                $nextLevel = $levels->where('name', $keys[$currentLevel->name])->first();
+                
+                if($nextLevel ) {
+                    Log::info('El grupo ' . $group->name . ' se movió al nivel ' . $nextLevel->name . ' venia del nivel ' . $currentLevel->name);
+                    $groups->name = $group->name += 100;
+                    $group->level_id = $nextLevel->id;
+                    $group->save();
+                }
             }
-            Log::info('El grupo ' . $group->name . ' se movió al nivel ' . $nextLevel->name . ' venia del nivel ' . $currentLevel->name);
+            DB::commit();
+            return response()->json(["message" => "Migración de semestres completada"], 200);
+        }catch(\Exception $e){
+            Log::error('Error al migrar los semestres: ' . $e->getMessage());
+            DB::rollBack();
+            return response()->json(["message" => "Error al migrar los semestres"], 500);
         }
-
-        return response()->json(["message" => "Migración de semestres completada"], 200);
 
     }
 }
